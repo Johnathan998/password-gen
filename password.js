1be68f00051f857b563518d79681033d01906b6d
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-01 14:23:01 -0700

    added function

---
+++ password.js
@@ -0,0 +1,14 @@
+function generate(length = 12) {
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+    var all = uppercase + lowercase + numbers + symbols;
+    var password = '';
+    for (var i = 0; i < length; i++) {
+        var character = Math.floor(Math.random() * all.length);
+        password += all.substring(character, character + 1);
+    }
+
+    return password;
+}
